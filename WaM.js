f86264ed064030233833c52ae9c5186c1c77d419
Brett Ellis <brett.ash.ellis@gmail.com>
2022-01-28 16:49:57 -0600

    Added intervals + other bs

---
+++ WaM.js
@@ -0,0 +1,45 @@
+let time = 0
+let gameactive = false
+document.getElementById("startthegame").addEventListener('click', function () {
+
+    gameactive = true
+    time = 30
+    document.getElementById("Timer").innerText = `Time: 30`
+    document.getElementById("startthegame").style.display = "none"
+
+    let timeinterval = setInterval(() => {
+
+        if (time > 0) {
+            time--
+            document.getElementById("Timer").innerText = `Time: ${time}`
+        }
+        else {
+            gameactive = false
+            clearInterval(timeinterval)
+            document.getElementById("Timer").innerText = `Times up!`
+
+        }
+
+        console.log(time)
+    }, 1000);
+    let moleInterval = setInterval(() => {
+        if (!gameactive) {
+            clearInterval(moleInterval)
+            return
+        }
+        let moles = document.querySelectorAll(".moleHoles")
+
+        let moleactive = Math.floor(Math.random() * moles.length)
+        moles[moleactive].classList.add("moles")
+
+        setTimeout(() => {
+            console.log(moleactive)
+            moles[moleactive].classList.remove("moles")
+        }, 750)
+    }, 500);
+
+    document.querySelectorAll(".moleHoles").forEach((element) => {
+        element.addEventListener('click')
+    })
+
+})
